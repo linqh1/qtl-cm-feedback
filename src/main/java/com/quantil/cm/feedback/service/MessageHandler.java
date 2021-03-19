9648f97a32532f2c6bb684eeb12b122146717d44
linqh1 <qihua.lin@quantil.com>
2021-03-19 15:50:20 +0800

    完成回调NGAPI代码

--- src/main/java/com/quantil/cm/feedback/service/MessageHandler.java
+++ src/main/java/com/quantil/cm/feedback/service/MessageHandler.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.quantil.cm.feedback.dto.PrefetchFeedbackMessage;
 import com.quantil.cm.feedback.dto.PurgeFeedbackMessage;
 import com.quantil.cm.feedback.dto.TaskMessage;
-import org.apache.http.HttpRequest;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
@@ -21,7 +23,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,10 +32,13 @@ public class MessageHandler implements MessageListenerConcurrently {
 
     private static Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
-    @Value("${httpclient.timeout.connect:10000}")
+    @Value("${cm.feedback.timeout.connect:10000}")
     private int connTimeout;
-    @Value("${httpclient.timeout.socket:20000}")
+    @Value("${cm.feedback.timeout.socket:20000}")
     private int socketTimeout;
+
+    @Value("cm.feedback.addr")
+    private String httpAddr;
 
     @Autowired
     private PurgeService purgeService;
@@ -56,7 +62,15 @@ public class MessageHandler implements MessageListenerConcurrently {
 
     @Override
     public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-        List<HttpRequest> requests = trans2Request(msgs);
+        List<HttpPut> requests = trans2Request(msgs);
+        try {
+            for (HttpPut request : requests) {
+                httpClient.execute(request);
+            }
+        }catch (Exception e) {
+            logger.error("execute http request failed", e);
+            return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+        }
         return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
     }
 
@@ -65,7 +79,7 @@ public class MessageHandler implements MessageListenerConcurrently {
      * @param msgs
      * @return
      */
-    private List<HttpRequest> trans2Request(List<MessageExt> msgs) {
+    private List<HttpPut> trans2Request(List<MessageExt> msgs) {
         List<TaskMessage> taskMessages = new ArrayList<>();
         for (MessageExt msg : msgs) {
             try {
@@ -74,15 +88,26 @@ public class MessageHandler implements MessageListenerConcurrently {
                 logger.error("parse MQ message:{} failed:{}",new String(msg.getBody()),e);
             }
         }
-        List<HttpRequest> result = new ArrayList<>();
+        List<HttpPut> result = new ArrayList<>();
+
         List<TaskMessage> prefetchMessage = taskMessages.stream().filter(m -> m.isPrefetch()).collect(Collectors.toList());
         List<PrefetchFeedbackMessage> prefetchFeedbackList = prefetchService.getFeedbackMessage(prefetchMessage);
         if (!prefetchFeedbackList.isEmpty()) {
-            HttpPost httpPost = new HttpPost();
+            result.add(buildHttpRequest("/internal/prefetches",new StringEntity(JSON.toJSONString(prefetchFeedbackList),"UTF-8")));
         }
 
         List<TaskMessage> purgeMessage = taskMessages.stream().filter(m -> !m.isPrefetch()).collect(Collectors.toList());
         List<PurgeFeedbackMessage> purgeFeedbackList = purgeService.getFeedbackMessage(purgeMessage);
+        if (!purgeFeedbackList.isEmpty()) {
+            result.add(buildHttpRequest("/internal/purges",new StringEntity(JSON.toJSONString(purgeFeedbackList),"UTF-8")));
+        }
         return result;
+    }
+
+    private HttpPut buildHttpRequest(String api, HttpEntity entity) {
+        HttpPut httpPut = new HttpPut(httpAddr + api);
+        httpPut.addHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        httpPut.setEntity(entity);
+        return httpPut;
     }
 }
